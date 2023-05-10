db0ee05d0dc22bb613e12cf159b8483dfcd25e0e
Alina Tudose <alinatudose@RC-MB2023008.local>
2023-05-10 22:50:19 +0200

    update title

--- app/page2.tsx
+++ app/page2.tsx
@@ -27,18 +27,30 @@ export default async function Home() {
   return (
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
+        <a
+          href="https://twitter.com/steventey/status/1613928948915920896"
+          target="_blank"
+          rel="noreferrer"
+          className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
+        >
+          <Twitter className="h-5 w-5 text-[#1d9bf0]" />
+          <p className="text-sm font-semibold text-[#1d9bf0]">
+            Introducing Precedent
+          </p>
+        </a>
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
-          <Balancer>Your pet's linkedIn</Balancer>
+          <Balancer>Building blocks for your Next project</Balancer>
         </h1>
         <p
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
         >
           <Balancer>
-            Have all your pet's information in one place to easily share it your vet, groomer, or pet sitter or one of your pet's fans.
+            An opinionated collection of components, hooks, and utilities for
+            your Next.js project.
           </Balancer>
         </p>
         <div
